c1dad87774aefb37abdfc07d2f735f806543054b
Silvan Mosberger <contact@infinisil.com>
2020-03-26 02:55:16 +0100

    pkgs-lib: Implement settings formats for JSON, INI, YAML and TOML

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -526,7 +526,7 @@ in
   writers = callPackage ../build-support/writers {};
 
   # lib functions depending on pkgs
-  inherit (import ../pkgs-lib { inherit lib pkgs; });
+  inherit (import ../pkgs-lib { inherit lib pkgs; }) formats;
 
   ### TOOLS
 
